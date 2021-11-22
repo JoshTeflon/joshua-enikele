80ca26c3de8075c2885fd06ea13187ea7e20692d
Joshua Enikele <joshua.enikele@sizach.ng>
2021-11-22 10:41:50 +0100

    footer and contact

--- components/landing_page/Landing.js
+++ components/landing_page/Landing.js
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import { useState } from 'react'
+import Link from 'next/link'
 
 import LandingImage from './LandingImage'
 
@@ -27,7 +27,7 @@ const Landing = (props) => {
                                 </svg>
                             </a>
                         </div>
-                        <a className="btn download_btn" href="https://drive.google.com/file/d/1gVri2INYRUSVbtVy77iyuOcViFHCU_Jc/view?usp=sharing" target="_blank" rel="noreferrer">
+                        <a className="btn download_btn" href="https://drive.google.com/file/d/1gVri2INYRUSVbtVy77iyuOcViFHCU_Jc/view?usp=sharing" target="_blank" rel="noopener noreferrer">
                             <div className="download_btn__text">Download CV</div>
                             <div className="download_btn__icon">
                                 <Image src={download} alt="download icon" />
